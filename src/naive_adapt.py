265255e8465e23de594ae5db2d4a365240b092c2
N1colaOu <n.ouzounov06@gmail.com>
2026-04-01 13:27:01 +0200

    Adaptive euler added

---
+++ src/naive_adapt.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#------------------------------------#
+#input your function here
+def f(t, y):
+    return y - 0.5*np.exp(t/2)*np.sin(5*t) + 5*np.exp(t/2)*np.cos(5*t)
+h = 1e-3
+t_start = 0.0
+t_end = 10.0
+y0 = 0.0
+#------------------------------------#
+def adjust_h(t_n, y_n, h, tol1 = 1e-4, tol2 = 2e-4):
+    A = f(t_n, y_n)
+    yE = y_n + h*A
+    B = f(t_n + h,  y_n + A*h)
+    yH = y_n + (A + B) * h / 2
+    delta = np.abs(yE-yH)
+    if delta > tol1:
+        h = h/2
+    elif delta < tol2:
+        h = h*2
+    return h
+def y_n1(tn, yn, h):
+    adjust_h(tn, yn, h)
+    return yn + h*f(tn, yn)
+
+t_span = np.abs(t_end - t_start)
+n = int(t_span/h)
+t = np.linspace(t_start, t_end, n)
+y = np.zeros(n, dtype=float)
+y[0] = y0
+for i in range(0, n-1):
+    y[i+1] = y_n1(t[i], y[i], h)
+
+
+fig, ax = plt.subplots(figsize = (5, 5), layout = 'constrained')
+ax.scatter(t, y, linewidths=0.05, edgecolors="none", marker='+')
+ax.set(xlim = [t_start, t_end], xlabel = "t", ylabel = "y(t)", title = "Adaptive Step Method")
+plt.show()

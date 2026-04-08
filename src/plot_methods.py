ef8a4cfa2a55393c318deed37b6cc12f29f8a15e
N1colaOu <n.ouzounov06@gmail.com>
2026-04-08 12:19:43 +0200

    Renamed the main file and added another to plot the second order

---
+++ src/plot_methods.py
@@ -0,0 +1,67 @@
+import euler_frw as efr
+import euler_bwd as ebw
+import heun as rk2
+import rk4 as rk4
+import naive_adapt as adp
+import to_compare as cmp
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(t, y):
+    return 20*np.cos(10*t)
+h = 1e-3
+t_start = 0.0
+t_end = 10.0
+y0 = 1.0
+
+
+euler_f_y = efr.get_sol(t_start, t_end, h, y0, f)
+euler_b_y = ebw.get_sol(t_start, t_end, h, y0, f)
+heun_rk2_y = rk2.get_sol(t_start, t_end, h, y0, f)
+rk4_y = rk4.get_sol(t_start, t_end, h, y0, f)
+adaptive = adp.get_sol(t_start, t_end, h, y0, f)
+adaptive_y = adaptive[0]
+adaptive_t = adaptive[1]
+solution = cmp.get_sol(t_start, t_end, h, y0, f)
+t = np.linspace(t_start, t_end, int((t_end-t_start)/h))
+
+
+plt.figure()
+#euler forward
+plt.subplot(321)
+plt.plot(t, euler_f_y)
+#plt.scatter(t, euler_f_y, linewidths=1e-1, marker="x")
+plt.title("Forward Euler")
+plt.xlim([t_start, t_end])
+#euler back2ward
+plt.subplot(322)
+plt.plot(t, euler_b_y)
+plt.title("Backward Euler")
+plt.xlim([t_start, t_end])
+#adaptive
+plt.subplot(323)
+plt.plot(adaptive_t, adaptive_y)
+plt.scatter(adaptive_t, adaptive_y, linewidths=0.06, marker="+", c='r')
+plt.title("Adaptive Method")
+plt.xlim([t_start, t_end])
+#heun
+plt.subplot(324)
+plt.plot(t, heun_rk2_y)
+plt.title("Heun/RK2")
+plt.xlim([t_start, t_end])
+#rk4
+plt.subplot(325)
+plt.plot(t, rk4_y)
+plt.title("RK4")
+plt.xlim([t_start, t_end])
+#solution to compare with
+plt.subplot(326)
+plt.plot(t, solution)
+plt.title("Most Accurate Version")
+plt.xlim([t_start, t_end])
+
+plt.subplots_adjust(top=0.87, bottom=0.08, left=0.10, right=0.95, hspace=0.5, wspace=0.35)
+plt.suptitle("Solution Comparison")
+plt.show()
+
+
---
+++ src/plot_second_order.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import solve_osc as osc
+
+#y'' + ay' + by = 0
+#Input paramters:
+y_0 = 0
+dy_0 = 1
+a = 0.5
+b = 2
+step = 1e-2
+t_start = 0
+t_end = 10
+
+
+t = np.linspace(t_start, t_end, int((t_end-t_start)/step))
+y = osc.solve(y_0, dy_0, a, b, t)
+plt.plot(t, y)
+plt.title("Damped Oscillations")
+plt.xlim([t_start, t_end])
+plt.show()
